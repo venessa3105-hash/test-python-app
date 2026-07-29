bec8763f8de37add51d2b5a6840094052262abe8
VenessaChrisy.S <venessa.chrisy-s-ext@hpe.com>
2026-07-29 21:29:37 +0000

    Fix SQL Injection vulnerability

--- sql_injection.py
+++ sql_injection.py
@@ -1,10 +1,11 @@
 import sqlite3
 
-username = input("Username: ")
-
 conn = sqlite3.connect("users.db")
 cursor = conn.cursor()
 
-query = "SELECT * FROM users WHERE username = ?"
+user_input = input("ID: ")
 
-cursor.execute(query, (username,))
+cursor.execute(
+    "SELECT * FROM users WHERE id = ?",
+    (user_input,)
+)
