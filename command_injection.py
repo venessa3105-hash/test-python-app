46a4c670f19021bd0d3d06052fb794e900d1f74e
VenessaChrisy.S <venessa.chrisy-s-ext@hpe.com>
2026-07-29 06:24:03 +0000

    Add vulnerable test cases

---
+++ command_injection.py
@@ -0,0 +1,5 @@
+import os
+
+user_input = input("Enter a filename: ")
+
+os.system("ls " + user_input)
---
+++ sql_injection.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+username = input("Username: ")
+
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
+
+query = "SELECT * FROM users WHERE username = '" + username + "'"
+
+cursor.execute(query)
